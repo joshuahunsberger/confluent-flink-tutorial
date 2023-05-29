4b6d413fb7434c97e96a48bd6bcb6e91c38e5893
Joshua Hunsberger <joshua@hunsberger.co>
2023-05-29 19:41:00 +0000

    Add Flink SQL for creating unbounded pageviews table

---
+++ create-pageviews-table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `pageviews` (
+  `url` STRING,
+  `user_id` STRING,
+  `browser` STRING,
+  `ts` TIMESTAMP(3)
+)
+WITH (
+  'connector' = 'faker',
+  'rows-per-second' = '100',
+  'fields.url.expression' = '/#{GreekPhilosopher.name}.html',
+  'fields.user_id.expression' = '#{numerify ''user_##''}',
+  'fields.browser.expression' = '#{Options.option ''chrome'', ''firefox'', ''safari'')}',
+  'fields.ts.expression' =  '#{date.past ''5'',''1'',''SECONDS''}'
+);
